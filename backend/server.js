9b59b3d7de7a15d45440623ff483905e832cb8e2
Mohit Singh <mohitkumar7492492@mail.com>
2026-09-07 16:59:31 +0530

    changed url of frontend in backend

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ await connectDB();
 const app = express();
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: process.env.CLIENT_URL || "https://learnwithease.onrender.com",
     credentials: true,
   }),
 );
